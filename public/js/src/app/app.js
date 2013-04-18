d3dabbabe3f9a83b0d66604a8425c1af7ed461ff
Kevin Reed <kevin.a.reed@gmail.com>
2013-04-18 02:35:20 -0400

    add node engine to package.json

--- public/js/src/app/app.js
+++ public/js/src/app/app.js
@@ -1,23 +1,31 @@
 define([
+
 	'views/ApplicationView',
 	'views/IndexView',
+
 	'controllers/ApplicationController',
 	'controllers/IndexController',
+
 	'src/app/router',
 	'routes/IndexRoute'
+
 ], function(
 	ApplicationView,
 	IndexView,
+
 	ApplicationController,
 	IndexController,
+
 	Router,
 	IndexRoute){
 
 	var App = {
 		ApplicationView: ApplicationView,
 		IndexView: IndexView,
+
 		ApplicationController: ApplicationController,
 		IndexController: IndexController,
+		
 		Router: Router,
 		IndexRoute: IndexRoute
 	};
